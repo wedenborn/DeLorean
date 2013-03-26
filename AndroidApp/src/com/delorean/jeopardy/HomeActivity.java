3a2bd4b55e4536ed015e1b392bf146a8fd3a1cd7
Andreas Stjerndal <anden42@gmail.com>
2013-03-26 18:56:05 +0100

    Gjorde ett ordentligt Android-projekt istället.

---
+++ AndroidApp/src/com/delorean/jeopardy/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.delorean.jeopardy;
+
+import java.util.Random;
+
+import com.example.jeapordytest.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class HomeActivity extends Activity {
+	
+	public final static String QUESTION_ID = "com.example.jeopardytest.questionID";
+	public final static String LOG_TAG = "www";
+	public final static int NUMBER_OF_QUESTIONS = 100;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+	
+	public void toRandomQuestion (View view) {
+		Intent intent = new Intent(this, QuestionActivity.class);
+    	int id = randomQuestionID();
+    	Log.d(LOG_TAG, String.valueOf(id));
+    	intent.putExtra(QUESTION_ID, id);
+    	startActivity(intent);
+	}
+	//
+	private int randomQuestionID () {
+		Random rand = new Random();
+		return rand.nextInt(NUMBER_OF_QUESTIONS);
+	}
+
+}
---
+++ AndroidApp/src/com/delorean/jeopardy/QuestionActivity.java
@@ -0,0 +1,36 @@
+package com.delorean.jeopardy;
+
+
+import com.example.jeapordytest.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class QuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_question);
+		
+		Intent intent = getIntent();
+		int questionID = intent.getIntExtra(HomeActivity.QUESTION_ID,-1);
+		
+		TextView questionText = (TextView) findViewById(R.id.question_id_text);
+		
+		Log.d(HomeActivity.LOG_TAG, String.valueOf(questionID));
+		questionText.setText("Question " + String.valueOf(questionID));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
